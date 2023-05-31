3c00fe53cc837a190c4508149ca24c6225f5d58b
raehan <raehan900809@gmail.com>
2023-06-01 00:10:55 +0900

    add: react router dom 라이브러리 설치하다

---
+++ src/router/index.tsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+        errorElement: <ErrorComponent />,
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "users/:userId",
+        // users 일때의 element가 없다면 이렇게 해도 되고
+        // users와 users/:id의 element가 다르다면 다시 children으로 나눠주면 된다.
+        element: <User />,
+        children: [
+          {
+            path: "followers",
+            element: <Followers />
+          }
+        ]
+      },
+    ],
+    errorElement: <NotFound />,
+    // errorElement: <ErrorComponent />
+
+  }
+])
+
+export default router;
