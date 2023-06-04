d96eaadf7218890b707db07a7d19adb09c351863
raehan <raehan900809@gmail.com>
2023-06-04 12:40:59 +0900

    modify: 너비에 따라 캐릭터 목록 열 갯수를 바꾸다

--- src/components/CharacterList/styels.tsx
+++ src/components/CharacterList/styels.tsx
@@ -8,4 +8,8 @@ export const CharacterListContainer = styled.ul`
   max-width: 768px;
   margin: 0 auto;
   padding: 0 20px;
+  
+  @media screen and (max-width: 767px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  } 
 `;
