0d1eeff912c84ab58a365052aec62baef9b4a911
raehan <raehan900809@gmail.com>
2023-06-06 22:01:40 +0900

    add: eslint, prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,33 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-    'no-unused-vars': 'off',
-    "react/react-in-jsx-scope": "off",
-  },
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:prettier/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint",
+      "react-refresh"
+    ],
+    "rules": {
+        'react-refresh/only-export-components': 'warn',
+        'no-unused-vars': 'off',
+        "react/react-in-jsx-scope": "off",
+    }
 }
